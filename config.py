b5727df1e123e7ee07bc5dafd4da132fb786895e
lelalomos <lelalomos@gmail.com>
2025-02-21 00:45:25 +0700

    update group stock

--- config.py
+++ config.py
@@ -1,37 +1,36 @@
-TICKET_LIST = ["AAPL","TSLA","NFLX","BNTX","AMZN","MSFT","META","NVDA","GOOGL","PYPL","CSCO","ADBE","INTU",
-        "LI","NEE","ON","NIU","ZM","MMM","DUOL","COIN","AVGO","ABNB","QCOM","LOGI","WDC","DBX","CFLT","AMD","ORCL","CRM","MDB","EBAY",
-        "IBM","BABA","SE","U","SPOT","WAL","RACE","ACN","HPQ","TSM","SBUX","NKE","XOM","DIS","BA","PLTR","SMCI","MSTR","INTC",
-        "CLSK","BAC","AMC","UNH","UBER","COST","PFE","CRWD","OXY","WMT","LLY","MCD","DELL","SNOW","SOFI","ASML","V","CELH","HIMS"
-        "JNJ","PG","ENPH","PEP","MA","ABBV","GRAB","KO","PDD","TMDX","ARM","MU","JPM", "PANW","CHWY","F","JD","MRNA","CVX","RBLX","MRK",
-        "RDDT","BIDU","RTX","PINS","WBD","HSY","HPE","TM","GTLB","NOC","BILI","MDLZ","K","WIX","MSI","NVO","PVH","CDNA","BOX","ANF","AMBA",
-        "S","BHP","TCOM","WDAY","INTU","NTES","A","SNPS","M","ADI","TJX","TGT","COTY","MDT","LOW","FN","EL","ZIM","GLOB", "EA", "RGTI"]
+# TICKET_LIST = ["AAPL","TSLA","NFLX","BNTX","AMZN","MSFT","META","NVDA","GOOGL","PYPL","CSCO","ADBE","INTU",
+#         "LI","NEE","ON","NIU","ZM","MMM","DUOL","COIN","AVGO","ABNB","QCOM","LOGI","WDC","DBX","CFLT","AMD","ORCL","CRM","MDB","EBAY",
+#         "IBM","BABA","SE","U","SPOT","WAL","RACE","ACN","HPQ","TSM","SBUX","NKE","XOM","DIS","BA","PLTR","SMCI","MSTR","INTC",
+#         "CLSK","BAC","AMC","UNH","UBER","COST","PFE","CRWD","OXY","WMT","LLY","MCD","DELL","SNOW","SOFI","ASML","V","CELH","HIMS"
+#         "JNJ","PG","ENPH","PEP","MA","ABBV","GRAB","KO","PDD","TMDX","ARM","MU","JPM", "PANW","CHWY","F","JD","MRNA","CVX","RBLX","MRK",
+#         "RDDT","BIDU","RTX","PINS","WBD","HSY","HPE","TM","GTLB","NOC","BILI","MDLZ","K","WIX","MSI","NVO","PVH","CDNA","BOX","ANF","AMBA",
+#         "S","BHP","TCOM","WDAY","INTU","NTES","A","SNPS","M","ADI","TJX","TGT","COTY","MDT","LOW","FN","EL","ZIM","GLOB", "EA", "RGTI"]
 
 # test
-# TICKET_LIST = ["AAPL","TSLA","NFLX"]
+TICKET_LIST = ["GOOGL","MSFT","META","NFLX","RDDT","ABNB"]
 
-# ADI
 SECTOR_GROUP = {
-        "software":["MSFT","NFLX","GOOGL","META","ADBE","ZM","DUOL","COIN","ABNB","CRM","MDB","EBAY",
-                    "BABA","U","SPOT","ACN","DIS","RDDT","BIDU","PINS","GTLB","WIX","BOX","AMBA","TCOM",
-                    "WDAY","GLOB"],
+        "software":["ADBE","DUOL","CRM","MDB",
+                    "U","SPOT","ACN","PINS","GTLB","WIX","BOX","AMBA",
+                    "WDAY","GLOB","MSFT","GOOGL"],
         "security":["CRWD","PANW","S"],
         "ecommerce":["EBAY","BABA","SE","PDD","JD"],
-        "community":["META","GOOGL","ZM","RDDT","MSI"],
-        "entertain":["NFLX","AMZN","DIS","AMC","BIDU","WBD","BILI"],
-        "cloud":["AMZN","GOOGL","DBX","CFLT","ORCL","IBM","CLSK","SNOW"],
-        "chip":["ON","AAPL","NVDA","AVGO","QCOM","AMD","TSM","SMCI","INTC","ASML","ARM","SNPS","RGTI"],
+        "community":["META","ZM","RDDT"],
+        "entertain":["NFLX","DIS","AMC","BIDU","WBD","BILI"],
+        "cloud":["DBX","CFLT","ORCL","IBM","CLSK","SNOW"],
+        "chip":["ON","AVGO","QCOM","AMD","TSM","SMCI","INTC","ASML","ARM","SNPS","RGTI"],
         "industries":["FN"],
-        "hardware":["LOGI","AMZN","MSFT","WDC","HPQ","INTC","AAPL","NVDA","AMD","DELL","MU","HPE","MSI"],
+        "hardware":["LOGI","WDC","HPQ","NVDA","DELL","MU","HPE","MSI"],
         "auto":["TSLA","LI","NIU","RACE","F","TM"],
         "bio":["BNTX"],
-        "finance":["PYPL","INTU","COIN","WAL","BAC","SOFI","V","MA","JPM","INTU"],
+        "finance":["PYPL","INTU","WAL","BAC","SOFI","V","MA","JPM"],
         "network":["CSCO"],
         "energy":["NEE","XOM", "OXY", "ENPH","CVX","BHP"],
-        "utility":["MMM","AMZN","NKE","COST","WMT","PG","PVH","ANF","M","TJX","COTY","LOW","EL"],
-        "phone":["AAPL", "GOOGL"],
+        "utility":["MMM","NKE","COST","WMT","PG","PVH","ANF","M","TJX","COTY","LOW","EL"],
+        "phone":["AAPL"],
         "study":["DUOL"],
-        "delivery":["UBER","GRAB","TGT","ZIM"],
-        "crypto":["COIN","MSTR","CLSK"],
+        "delivery":["UBER","GRAB","TGT","ZIM","AMZN"],
+        "crypto":["COIN","MSTR"],
         "rent":["ABNB","TCOM"],
         "food":["SBUX","MCD","CELH","PEP","KO","HSY","MDLZ","K"],
         "pet":["CHWY"],
@@ -39,6 +38,35 @@ SECTOR_GROUP = {
         "game":["RBLX","U","EA","NTES"],
         "health":["UNH","PFE","LLY","HIMS","JNJ", "ABBV","TMDX","MRNA","MRK","NVO","CDNA","A","MDT"],
         "astros":["RTX","NOC"]
+}
+
+WEIGHT_GROUP = {
+    "software":1,
+    "security":1,
+    "ecommerce":1,
+    "community":1,
+    "entertain":1,
+    "cloud":1,
+    "chip":1,
+    "industries":1,
+    "hardware":1,
+    "auto":1,
+    "bio":1,
+    "finance":1,
+    "network":1,
+    "energy":1,
+    "utility":1,
+    "phone":1,
+    "study":1,
+    "delivery":1,
+    "crypto":1,
+    "rent":1,
+    "food":1,
+    "pet":1,
+    "aircraft":1,
+    "game":1,
+    "health":1,
+    "astros":1
 }
 
 INDICATOR_LIST = ['rsi_14','stochrsi_14','vwma_20','ema_200', 'ema_50', 'ema_100', 'macd', 'ichimoku']
@@ -61,17 +89,5 @@ ICHIMOKU_UP = 1
 ICHIMOKU_DOWN = 0.5
 
 # lstm model config
-LSTM_PARAMS = {'output_size': 139,
-                'embedding_dim_stock': 49,
-                'embedding_dim_group': 15,
-                'embedding_dim_day': 38,
-                'embedding_dim_month': 35,
-                'first_layer_hidden_size': 186,
-                'first_layer_size': 2,
-                'second_layer_hidden_size': 405,
-                'second_layer_size': 5,
-                'third_layer_hidden_size': 202,
-                'third_layer_size': 1,
-                'dropout': 0.16918304089580144,
-                'delta': 0.10013449995372502}
+LSTM_PARAMS = {'output_size': 226, 'embedding_dim_stock': 6, 'embedding_dim_group': 10, 'embedding_dim_day': 44, 'embedding_dim_month': 18, 'first_layer_hidden_size': 182, 'first_layer_size': 1, 'second_layer_hidden_size': 376, 'second_layer_size': 1, 'third_layer_hidden_size': 216, 'third_layer_size': 1, 'dropout': 0.23667559523715945, 'delta': 0.10007115305567968}
 
